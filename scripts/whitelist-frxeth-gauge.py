219f434f81f55a8d37cc6835013caa1df3d1a267
ZeframLou <zeframlou@gmail.com>
2023-02-27 10:23:02 -0800

    add whitelist-frxeth-gauge.py

---
+++ scripts/whitelist-frxeth-gauge.py
@@ -0,0 +1,51 @@
+# brownie run scripts/whitelist-frxeth-gauge.py --network mainnet-fork
+
+from ape_safe import ApeSafe
+import json
+from brownie import Contract
+from brownie.convert import to_string
+
+def main():
+    # configs
+    gov_safe = ApeSafe("0x9a8FEe232DCF73060Af348a1B62Cdb0a19852d13")
+
+    # abis
+    with open('scripts/abi/GaugeController.json') as f:
+        gauge_controller_abi = json.loads(f.read())
+    with open('scripts/abi/TimelessLiquidityGauge.json') as f:
+        gauge_abi = json.loads(f.read())
+
+    # config
+    gauges = ["0x4Bf0082080d937897330BAB735c2Baa99FF16F19"]
+
+    # contracts
+    gauge_controller = Contract.from_abi(
+        "GaugeController", "0x901c8aA6A61f74aC95E7f397E22A0Ac7c1242218", gauge_controller_abi, gov_safe.account)
+
+    ######################################################################
+    # Whitelist gauges
+    ######################################################################
+
+    for gauge in gauges:
+        gauge_controller.add_gauge(gauge, 0, 1)
+
+    ######################################################################
+    # Update gauge tokenless_production
+    ######################################################################
+
+    for gauge in gauges:
+        gauge_contract = Contract.from_abi("TimelessLiquidityGauge", gauge, gauge_abi, gov_safe.account)
+        gauge_contract.set_tokenless_production(10) # 10x max boost
+
+    ######################################################################
+    # Submit transaction to gov Gnosis Safe
+    ######################################################################
+
+    # generate safe tx
+    safe_tx = gov_safe.multisend_from_receipts()
+
+    # sign safe tx
+    gov_safe.sign_with_frame(safe_tx).hex()
+
+    # post tx
+    gov_safe.post_transaction(safe_tx)
