ab1168c0bd78981621b6e1ae41c1ca031aedda8a
ZeframLou <zeframlou@gmail.com>
2023-03-06 10:55:42 -0800

    update tip-03.py with correct address

--- scripts/tip-03.py
+++ scripts/tip-03.py
@@ -9,7 +9,7 @@ def main():
     print(ape_safe.__file__)
 
     # configs
-    dev_safe = ApeSafe("0x39D719fE517Bd73F414A90ed3A14527a5737c8e5")
+    eng_safe = ApeSafe("0x798E6af7658A1f20e6B2A5c780D8577D4AEAF7dA")
     gov_safe = ApeSafe("0x9a8FEe232DCF73060Af348a1B62Cdb0a19852d13")
 
     # abis
@@ -22,11 +22,11 @@ def main():
 
     usdc = gov_safe.contract("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
     usdc_amount = int(447000 * 1e6)
-    usdc.transfer(dev_safe.address, usdc_amount)
+    usdc.transfer(eng_safe.address, usdc_amount)
 
     lit = gov_safe.contract("0xfd0205066521550D7d7AB19DA8F72bb004b4C341")
     lit_amount = int(1e7 * 1e18)
-    lit.transfer(dev_safe.address, lit_amount)
+    lit.transfer(eng_safe.address, lit_amount)
     
     ######################################################################
     # Create LlamaPay stream of 1043000 USDC from 2023/3/5 to 2024/2/29
@@ -39,7 +39,7 @@ def main():
     usdc_amount = int(1043000 * 1e6)
 
     usdc.approve(llamapay_factory.address, usdc_amount)
-    llamapay_factory.deploy_vesting_contract(usdc.address, dev_safe.address, usdc_amount, vesting_duration, vesting_start)
+    llamapay_factory.deploy_vesting_contract(usdc.address, eng_safe.address, usdc_amount, vesting_duration, vesting_start)
 
     ######################################################################
     # Submit transaction to dev Gnosis Safe
