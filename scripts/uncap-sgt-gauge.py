be425c1c9b2cf67c60b23cbc10e30a8b104ecff6
ZeframLou <zeframlou@gmail.com>
2023-04-07 12:47:43 -0700

    add uncap-sgt-gauge.py

---
+++ scripts/uncap-sgt-gauge.py
@@ -0,0 +1,35 @@
+# brownie run scripts/uncap-sgt-gauge.py --network mainnet-fork
+
+from ape_safe import ApeSafe
+from brownie import Contract
+import json
+
+def main():
+    # configs
+    gov_safe = ApeSafe("0x9a8FEe232DCF73060Af348a1B62Cdb0a19852d13")
+
+    # abis
+    with open('scripts/abi/TimelessLiquidityGauge.json') as f:
+        gauge_abi = json.loads(f.read())
+
+    ######################################################################
+    # Uncap SGT-WETH gauge
+    ######################################################################
+
+    gauge = "0x3aCFa22E2d6573C15298A19C0e51506248696DeB"    
+    gauge_contract = Contract.from_abi("TimelessLiquidityGauge", gauge, gauge_abi, gov_safe.account)
+    weight_cap = int(1e18) # 100%
+    gauge_contract.setRelativeWeightCap(weight_cap)
+
+    ######################################################################
+    # Submit transaction to gov Gnosis Safe
+    ######################################################################
+
+    # generate safe tx
+    safe_tx = gov_safe.multisend_from_receipts()
+
+    # sign safe tx
+    gov_safe.sign_with_frame(safe_tx).hex()
+
+    # post tx
+    gov_safe.post_transaction(safe_tx)
