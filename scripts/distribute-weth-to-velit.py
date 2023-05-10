d5dd0dc1f47cdf6f4c770e3ca4fd5f0a141dd496
ZeframLou <zeframlou@gmail.com>
2023-05-10 11:07:24 -0700

    update olit distro script

--- scripts/distribute-weth-to-velit.py
+++ scripts/distribute-weth-to-velit.py
@@ -3,6 +3,8 @@
 from ape_safe import ApeSafe
 from brownie import Contract
 import json
+import time
+import requests
 
 def main():
     # configs
@@ -16,10 +18,37 @@ def main():
 
     weth_amount = int(7.8865072917 * 1e18)
     weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    subgraph_endpoint = "https://api.thegraph.com/subgraphs/name/bunniapp/olit-redemption"
 
     # contracts
     weth_contract = Contract.from_abi("WETH", weth, erc20_abi, gov_safe.account)
     fee_distributor = Contract.from_abi("FeeDistributor", "0x951f99350d816c0E160A2C71DEfE828BdfC17f12", fee_distributor_abi, gov_safe.account)
+
+    ######################################################################
+    # Fetch this epoch's oLIT redemption amount
+    ######################################################################
+
+    week = 7 * 24 * 60 * 60
+    epoch_start = int(time.time() // week * week)
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    query = '''
+    query Query($start: Int!) {
+        weeks(where: {startTimestamp: $start}) {
+            totalWethRedeemed
+        }
+    }
+    '''
+    payload = {"query": query, "variables": {"start": epoch_start}}
+    response = requests.post(subgraph_endpoint, json=payload, headers=headers)
+
+    if response.status_code == 200:
+        weth_redeemed = int(response.json()["data"]["weeks"][0]["totalWethRedeemed"]) // 2
+        weth_amount += weth_redeemed
+
+    print(f"Redeemed WETH to distribute: {weth_redeemed / 1e18}")
+    print(f"Total to distribute: {weth_amount / 1e18}")
 
     ######################################################################
     # Distribute WETH
